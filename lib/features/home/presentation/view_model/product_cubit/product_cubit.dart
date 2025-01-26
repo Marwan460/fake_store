af72ecbb68abc645cbabd47910d88fd43de451a4
Marwan <marwanyousry52@gmail.com>
2025-01-26 16:18:25 +0200

    Create product methode.

--- lib/features/home/presentation/view_model/product_cubit/product_cubit.dart
+++ lib/features/home/presentation/view_model/product_cubit/product_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:fake_store/features/home/data/models/data_model/product_model.dart';
+import 'package:fake_store/features/home/data/repos/home_repo.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'product_state.dart';
 
 class ProductCubit extends Cubit<ProductState> {
-  ProductCubit() : super(ProductInitial());
+  final HomeRepo homeRepo;
+
+  ProductCubit(this.homeRepo) : super(ProductInitial());
+
+  Future<void> fetchProducts() async {
+    emit(ProductLoading());
+    var result = await homeRepo.fetchData();
+    result.fold((failure) {
+      emit(ProductFailure(failure.toString()));
+    }, (products) {
+      emit(ProductSuccess(products));
+    });
+  }
 }
